2a2456f2a7ecb776c795993b88b08d6e4454545a
nur-alam <nuralam862@gmail.com>
2026-05-16 00:51:58 +0600

    style: update Switch component for improved UI
    - Increased height and width of the switch for better visibility.
    - Enhanced focus ring styling for accessibility.
    - Updated background colors for checked and unchecked states for improved contrast.

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -8,7 +8,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<SwitchPrimitives.Root
 		className={cn(
-			'vt-peer vt-inline-flex vt-h-5 vt-w-9 vt-shrink-0 vt-cursor-pointer vt-items-center vt-rounded-full vt-border-2 vt-border-transparent vt-shadow-sm vt-transition-colors focus-visible:vt-outline-none focus-visible:vt-ring-2 focus-visible:vt-ring-ring focus-visible:vt-ring-offset-2 focus-visible:vt-ring-offset-background disabled:vt-cursor-not-allowed disabled:vt-opacity-50 data-[state=checked]:vt-bg-primary data-[state=unchecked]:vt-bg-input',
+			'vt-peer vt-inline-flex vt-h-6 vt-w-11 vt-shrink-0 vt-cursor-pointer vt-items-center vt-rounded-full vt-border-2 vt-border-transparent vt-transition-colors focus-visible:vt-outline-none focus-visible:vt-ring-4 focus-visible:vt-ring-blue-300 disabled:vt-cursor-not-allowed disabled:vt-opacity-50 data-[state=checked]:vt-bg-blue-600 data-[state=unchecked]:vt-bg-[#c7c6c6]',
 			className,
 		)}
 		{...props}
@@ -16,7 +16,7 @@ const Switch = React.forwardRef<
 	>
 		<SwitchPrimitives.Thumb
 			className={cn(
-				'vt-pointer-events-none vt-block vt-h-4 vt-w-4 vt-rounded-full vt-bg-background vt-shadow-lg vt-ring-0 vt-transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0',
+				'vt-pointer-events-none vt-block vt-h-5 vt-w-5 vt-rounded-full vt-bg-white vt-border vt-border-gray-300 vt-shadow-lg vt-ring-0 vt-transition-transform data-[state=checked]:vt-translate-x-5 data-[state=unchecked]:vt-translate-x-0',
 			)}
 		/>
 	</SwitchPrimitives.Root>
